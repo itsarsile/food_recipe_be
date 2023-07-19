ad23dc66f8e7d081b4a9ba4290384db442de9408
Arsile <ajiprio@hotmail.com>
2023-07-19 12:51:07 +0700

    Express app initialized

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable new-cap */
 require('dotenv').config
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const createError = require('http-error');
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const createError = require('http-errors')
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors())
+app.use(helmet())
+app.use(xss())
+
+app.all('*', (req, res, next) => {
+  next(new createError.NotFound())
+})
+
+app.use((err, req, res, next) => {
+  const messageError = err.message || 'Internal server error'
+  const statusCode = err.status || 500
+
+  res.status(statusCode).json({
+    message: messageError
+  })
+})
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
