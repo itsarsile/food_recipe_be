6784e5583579820c932f9360052ff581312a3ee8
Arsile <ajiprio@hotmail.com>
2023-07-20 00:15:51 +0700

    remove morgan

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /* eslint-disable new-cap */
 require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const xss = require('xss-clean')
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
-app.use(morgan('combined'))
+// app.use(morgan('dev'))
 app.use(cors())
 app.use(helmet())
 app.use(xss())
