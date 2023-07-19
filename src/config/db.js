d6f7957eb070761a191bb049784f83fd095d2e3f
Arsile <ajiprio@hotmail.com>
2023-07-19 20:19:31 +0700

    added recipe models, controllers & common helper

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg')
 
 const pool = new Pool({
-    user: process.env.PGUSERNAME,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
+  user: process.env.PGUSERNAME,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
 })
 
 module.exports = pool
---
+++ src/models/recipe.models.js
@@ -0,0 +1,29 @@
+const Pool = require('../config/db')
+
+const getAllRecipes = () => {
+  return Pool.query('SELECT * FROM recipes;')
+}
+
+const getRecipesById = (recipesId) => {
+  return Pool.query(`SELECT * from recipes WHERE id = ${recipesId}`)
+}
+
+const createRecipeByUserId = (userId, title, photo, details, video) => {
+  const query = {
+    text: 'INSERT INTO recipes (user_id, photo, title, details, video) VALUES ($1, $2, $3, $4, $5)',
+    values: [userId, photo, title, details, video]
+  }
+  return Pool.query(query)
+}
+
+const updateRecipeByUserId = (userId, recipeId, updatedData) => {
+  const { photo, title, details, video } = updatedData
+
+  const query = {
+    text: 'UPDATE recipes SET photo = $1, title = $2, details = $3, video = $4 WHERE UserID = $5 AND ID = $6',
+    values: [photo, title, details, video, userId, recipeId]
+  }
+  return Pool.query(query)
+}
+
+module.exports = { getAllRecipes, getRecipesById, createRecipeByUserId, updateRecipeByUserId }
