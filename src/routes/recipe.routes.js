5a68263e42e9e4a68a67f5724c74d7e71031955d
Arsile <ajiprio@hotmail.com>
2023-07-21 10:10:58 +0700

    Recipe API: Upload key name change

--- src/routes/recipe.routes.js
+++ src/routes/recipe.routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const recipeController = require('../controllers/recipe.controller')
+const { upload } = require('../middleware/upload')
 
 router
   .get('/', recipeController.getAllRecipes)
   .get('/:recipeId', recipeController.getRecipesById)
-  .post('/', recipeController.createRecipe)
+  .post('/',
+    upload.single('recipeImage'),
+    recipeController.createRecipe)
   .put('/:recipeId', recipeController.updateRecipe)
 
 module.exports = router
