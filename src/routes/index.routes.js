e23553c7517260916cab9a1ba27794a184b5ed0e
Arsile <ajiprio@hotmail.com>
2023-07-23 20:40:22 +0700

    Recipe API: Unprotect the routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,9 +2,9 @@ const express = require('express')
 const userRouter = require('./user.routes')
 const recipeRouter = require('./recipe.routes')
 const router = express.Router()
-const { protect } = require('../middleware/auth')
+// const { protect } = require('../middleware/auth')
 
-router.use('/recipes', protect, recipeRouter)
-router.use('/users', protect, userRouter)
+router.use('/recipes', recipeRouter)
+router.use('/users', userRouter)
 
 module.exports = router
