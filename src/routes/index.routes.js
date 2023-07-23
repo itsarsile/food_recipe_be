3b23e343cdcdc6c305132903d9b227a762f25f79
Arsile <ajiprio@hotmail.com>
2023-07-23 20:19:34 +0700

    Recipes API: Fixed typo

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,8 +2,9 @@ const express = require('express')
 const userRouter = require('./user.routes')
 const recipeRouter = require('./recipe.routes')
 const router = express.Router()
+const { protect } = require('../middleware/auth')
 
-router.use('/recipes', recipeRouter)
-router.use('/users', userRouter)
+router.use('/recipes', protect, recipeRouter)
+router.use('/users', protect, userRouter)
 
 module.exports = router
